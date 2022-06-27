20da784d1af783058f5270ccd617348d1c111cf1
Marco Molteni <marco.molteni@pix4d.com>
2022-06-27 12:39:26 +0200

    ci: test-get: add test to validate that files are uploaded/downloaded
    Closes: PCI-2568

--- ci/tasks/test-get.sh
+++ ci/tasks/test-get.sh
@@ -1,11 +1,29 @@
 #! /bin/sh
 
-set -ex
+set -e
 
-echo "Directory listing"
-ls -R bucket.s3simple
+cd bucket.s3simple
 
+echo
+echo "All files downloaded from the bucket:"
+ls -R -1
 
-echo "use snapshot to compare with the directory listing above"
-cat bucket.s3simple/snapshot
+echo
+echo "Contents of snapshot file downloaded from the bucket:"
+echo
+cat snapshot
+
+# Read the snapshot file we found in the bucket and confirm that all the files are present.
+echo
+echo "Validating that the files in snapshot have been downloaded:"
+echo
+while IFS= read -r path
+do
+    if [ -f "$path" ]; then
+        echo "File exists: $path"
+    else
+        echo "Error: file does not exist: $path"
+        exit 1
+    fi
+done < snapshot
 
--- ci/tasks/test-put.sh
+++ ci/tasks/test-put.sh
@@ -11,14 +11,23 @@ timestamp=$(date +%Y-%m-%d_%H:%M:%S)
 
 # Create some files and directories to be uploaded to the bucket by a resource put and
 # then to be downloaded by a resource get, so that they can be compared.
+echo
+echo "Creating files to be uploaded to the bucket"
 touch out/witness-0-$timestamp
 touch out/dir1/witness-1-$timestamp
 touch out/dir1/dir2/witness-2-$timestamp
 
 # The test in job test-get will use `snapshot` to assert no diffs are present.
-# We create snapshot beforehand so that it will be included in the list.
-touch out/snapshot
-(cd out && ls -R) > out/snapshot
+echo
+echo "Making a list of the files (snapshot)"
+cat > out/snapshot << EOB
+witness-0-$timestamp
+dir1/witness-1-$timestamp
+dir1/dir2/witness-2-$timestamp
+EOB
 
-echo "Verify manually in the s3simple get that the following directories and files are uploaded:"
+echo
+echo "The test-get job should validate that the following files are present:"
+echo
 cat out/snapshot
+
